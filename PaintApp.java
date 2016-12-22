72502b31c778a32e14e4a1ed0c6ab27dcc12fbc8
kushtrimmh <kushtrimmh13@gmail.com>
2016-12-22 22:40:15 +0100

    Finish

--- PaintApp.java
+++ PaintApp.java
@@ -1,8 +1,17 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.*;
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.BorderLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.FocusEvent;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+import javax.imageio.ImageIO;
 
 public class PaintApp {
 
@@ -26,6 +35,59 @@ public class PaintApp {
 
 	public static void main(String[] args) {
 		new PaintApp();
+	}
+
+	/* Saves the drawing created in the drawing panel */
+	private void save() {
+		// Create the BufferedImage instance
+		BufferedImage image = new BufferedImage(paint.getWidth(), 
+												paint.getHeight(), 
+												BufferedImage.TYPE_INT_RGB);
+		Graphics2D g = image.createGraphics();
+		paint.paint(g);
+
+		// Chose a filename for the image
+		JFileChooser fch = new JFileChooser();
+		fch.showSaveDialog(frame);
+		File fl = fch.getSelectedFile();
+
+		// If filename was not choosed
+		// do nothing and return
+		if (fl == null) {
+			return;
+		}
+
+		try {
+			// Save the file to the choosen name
+			ImageIO.write(image, "png", fl);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	/* Loads an image to display it on the Paint panel */
+	private void load() {
+		// Get the image we want to load
+		JFileChooser fch = new JFileChooser();
+		// Add filter to open only png files
+		fch.setFileFilter(new FileNameExtensionFilter("PNG Files", "png"));
+		fch.showOpenDialog(frame);
+
+		File fl = fch.getSelectedFile();
+		if (fl == null) {
+			return;
+		}
+
+		// Read the image and pass it into the
+		// paint panel to diplay it
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(fl);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		paint.displayImage(img);
 	}
 
 	private void setGUI() {
@@ -52,20 +114,25 @@ public class PaintApp {
 
 		// Save menu item
 		JMenuItem fileSaveItem = new JMenuItem("Save");
+		fileSaveItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				save();
+			}
+		});
 
 		// Open menu item
 		JMenuItem fileOpenItem = new JMenuItem("Open");
+		fileOpenItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				load();
+			}
+		});
 
 		// Exit menu item
 		JMenuItem fileExitItem = new JMenuItem("Exit");
-		fileExitItem.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				frame.setVisible(false);
-				frame.dispose();
-				System.exit(0);
-			}
-		});
+		fileExitItem.addActionListener(new ExitAction());
 
 
 		fileMenu.add(fileSaveItem);
@@ -86,13 +153,107 @@ public class PaintApp {
 	* for buttons and text fields in the toolbar.
 	*/
 	public void buildToolbar() {
-		JToolBar toolBar = new JToolBar();
+		JToolBar toolbar = new JToolBar();
+		toolbar.setLayout(new FlowLayout(FlowLayout.LEADING));
+		toolbar.setFloatable(false);
 
-		JTextField sizeField = new JTextField();
+		// Add the exit button
+		JButton exitBtn = new JButton();
+		exitBtn.addActionListener(new ExitAction());
+		exitBtn.setIcon(new ImageIcon("images/exitimg.png"));
+		exitBtn.setToolTipText("Exit");
+		
+		toolbar.add(exitBtn);
 
-		toolBar.add(sizeField);
+		// Clear painting panel button
+		JButton clearButton = new JButton("Clear");
+		clearButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				paint.clean();
+			}
+		});
 
-		frame.add(toolBar, BorderLayout.NORTH);
+		toolbar.add(clearButton);
+
+		// Change color button
+		JButton colorButton = new JButton();
+		colorButton.setIcon(new ImageIcon("images/color.png"));
+		colorButton.setToolTipText("Choose Color");
+
+		colorButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Color c = JColorChooser.showDialog(frame, "Select Line Color", 
+														Color.WHITE);
+				// If the returned color is not null
+				// set the line color as the selected color
+				if (c != null) {
+					paint.setColor(c);
+				}
+			}
+		});
+
+		toolbar.add(colorButton);
+
+		// Pencil Button
+		JButton pencilButton = new JButton();
+		pencilButton.setIcon(new ImageIcon("images/pencil.png"));
+		pencilButton.addActionListener(new ChangeColorListener(Color.BLACK));
+
+		toolbar.add(pencilButton);
+
+		// Eraser Button
+		JButton eraserButton = new JButton();
+		eraserButton.setIcon(new ImageIcon("images/eraser.png"));
+		eraserButton.addActionListener(new ChangeColorListener(Color.WHITE));
+
+		toolbar.add(eraserButton);
+
+		// Set focus event for the sizeField so once
+		// the focus is out of this field the size of 
+		// the line width is changed automatically
+		JTextField sizeField = new JTextField("Size", 10);
+
+		sizeField.addFocusListener(new FocusListener() {
+			@Override
+			public void focusLost(FocusEvent e) {
+				// Try to change the text we got into float
+				try {
+					float width = Float.parseFloat(sizeField.getText());
+					paint.setLineWidth(width);
+				} catch (NumberFormatException ex) {}
+			}
+
+			@Override
+			public void focusGained(FocusEvent e) {}
+		});
+		toolbar.add(sizeField);
+
+		frame.add(toolbar, BorderLayout.NORTH);
+		frame.requestFocus();
 	}
 
+	// Listener classes
+	private class ChangeColorListener implements ActionListener {
+		Color color;
+
+		public ChangeColorListener(Color c) {
+			color = c;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			paint.setColor(color);
+		}
+	}
+
+	private class ExitAction implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			frame.setVisible(false);
+			frame.dispose();
+			System.exit(0);
+		}
+	}
 }
--- PaintPanel.java
+++ PaintPanel.java
@@ -1,21 +1,66 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.awt.image.BufferedImage;
 
 class PaintPanel extends JPanel implements MouseMotionListener, MouseListener{
-	ArrayList<ArrayList<Point>> pointList;
-	ArrayList<Point> currentList;
+	private ArrayList<ArrayList<Point>> pointList;
+	private ArrayList<Point> currentList;
+	private ArrayList<Float> lineWidthList;
+	private ArrayList<Color> colorList;
+
+	private float lineWidth;
+	private Color lineColor;
+	private BufferedImage loadImage;
+	private boolean firstTimeLoading;
 
 	public PaintPanel() {
 		// Add the mouse listner
 		setBackground(Color.WHITE);
+		setFocusable(true);
 		addMouseListener(this);
 		addMouseMotionListener(this);
 
+		// Initialize the lists that holds all drawn points
+		// colors, and linewidths
+		resetLists();
+
+		// Initialize settings
+		lineWidth = 1;
+		lineColor = Color.BLACK;
+		loadImage = null;
+		firstTimeLoading = false;
+	}
+
+	private void resetLists() {
 		// The list that holds all drawn points
 		pointList = new ArrayList<>();
+		lineWidthList = new ArrayList<>();
+		colorList = new ArrayList<>();
 		currentList = null;
+
+	}
+
+	public void setLineWidth(float width) {
+		lineWidth = width;
+	}
+
+	public void setColor(Color c) {
+		lineColor = c;
+	}
+
+	public void clean() {
+		// Clean the background
+		loadImage = null;
+		resetLists();
+		repaint();
+	}
+
+	public void displayImage(BufferedImage toLoad) {
+		loadImage = toLoad;
+		firstTimeLoading = true;
+		repaint();
 	}
 
 	@Override
@@ -31,7 +76,12 @@ class PaintPanel extends JPanel implements MouseMotionListener, MouseListener{
 
 	@Override
 	public void mousePressed(MouseEvent e) {
+		requestFocusInWindow();
 		currentList = new ArrayList<>();
+		// Add the current line width
+		// and the current color
+		lineWidthList.add(lineWidth);
+		colorList.add(lineColor);
 
 		pointList.add(currentList);
 	}
@@ -53,13 +103,35 @@ class PaintPanel extends JPanel implements MouseMotionListener, MouseListener{
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		Graphics2D gg = (Graphics2D) g;
+
+		// Check to see if an image is going to be loaded
+		// if true, reset the lists and display the image
+		// into the panel
+		if (loadImage != null) {
+			if (firstTimeLoading) {
+				resetLists();
+				firstTimeLoading = false;
+			}
+
+			gg.drawImage(loadImage, 0, 0, this);
+		}
+
 		// Draw all the points
+		int index = 0;
 		for (ArrayList<Point> lst: pointList) {
 			Point last = null;
+			// Get the line width for the current list
+			// of drawn points
+			float width = lineWidthList.get(index);
+			Color color = colorList.get(index++);
+			// Set the line width and color
+			gg.setStroke(new BasicStroke(width));
+			gg.setColor(color);
 			for (Point p: lst) {
 				// g.drawRect(p.x, p.y, 1, 1);
 				if (last != null) {
-					g.drawLine(last.x, last.y, p.x, p.y);
+					gg.drawLine(last.x, last.y, p.x, p.y);
 				}
 				last = p;
 			}
