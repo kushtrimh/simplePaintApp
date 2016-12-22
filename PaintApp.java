9426d18421a78c6bd5a0f8d1f17372d89504e397
kushtrimmh <kushtrimmh13@gmail.com>
2016-12-22 22:49:09 +0100

    Finish

--- PaintApp.java
+++ PaintApp.java
@@ -15,245 +15,245 @@ import javax.imageio.ImageIO;
 
 public class PaintApp {
 
-	private JFrame frame;
-	private PaintPanel paint;
-	private JPanel mainPanel;
+    private JFrame frame;
+    private PaintPanel paint;
+    private JPanel mainPanel;
 
-	public PaintApp() {
-		frame = new JFrame();
-		mainPanel = new JPanel();
-		paint = new PaintPanel();
+    public PaintApp() {
+        frame = new JFrame();
+        mainPanel = new JPanel();
+        paint = new PaintPanel();
 
-		setGUI();
+        setGUI();
 
-		// Set the frame options
-		frame.setSize(960, 640);
-		frame.setTitle("PaintApp");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-	}
+        // Set the frame options
+        frame.setSize(960, 640);
+        frame.setTitle("PaintApp");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 
-	public static void main(String[] args) {
-		new PaintApp();
-	}
+    public static void main(String[] args) {
+        new PaintApp();
+    }
 
-	/* Saves the drawing created in the drawing panel */
-	private void save() {
-		// Create the BufferedImage instance
-		BufferedImage image = new BufferedImage(paint.getWidth(), 
-												paint.getHeight(), 
-												BufferedImage.TYPE_INT_RGB);
-		Graphics2D g = image.createGraphics();
-		paint.paint(g);
+    /* Saves the drawing created in the drawing panel */
+    private void save() {
+        // Create the BufferedImage instance
+        BufferedImage image = new BufferedImage(paint.getWidth(), 
+                                                paint.getHeight(), 
+                                                BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = image.createGraphics();
+        paint.paint(g);
 
-		// Chose a filename for the image
-		JFileChooser fch = new JFileChooser();
-		fch.showSaveDialog(frame);
-		File fl = fch.getSelectedFile();
+        // Chose a filename for the image
+        JFileChooser fch = new JFileChooser();
+        fch.showSaveDialog(frame);
+        File fl = fch.getSelectedFile();
 
-		// If filename was not choosed
-		// do nothing and return
-		if (fl == null) {
-			return;
-		}
+        // If filename was not choosed
+        // do nothing and return
+        if (fl == null) {
+            return;
+        }
 
-		try {
-			// Save the file to the choosen name
-			ImageIO.write(image, "png", fl);
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
-	}
+        try {
+            // Save the file to the choosen name
+            ImageIO.write(image, "png", fl);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
-	/* Loads an image to display it on the Paint panel */
-	private void load() {
-		// Get the image we want to load
-		JFileChooser fch = new JFileChooser();
-		// Add filter to open only png files
-		fch.setFileFilter(new FileNameExtensionFilter("PNG Files", "png"));
-		fch.showOpenDialog(frame);
+    /* Loads an image to display it on the Paint panel */
+    private void load() {
+        // Get the image we want to load
+        JFileChooser fch = new JFileChooser();
+        // Add filter to open only png files
+        fch.setFileFilter(new FileNameExtensionFilter("PNG Files", "png"));
+        fch.showOpenDialog(frame);
 
-		File fl = fch.getSelectedFile();
-		if (fl == null) {
-			return;
-		}
+        File fl = fch.getSelectedFile();
+        if (fl == null) {
+            return;
+        }
 
-		// Read the image and pass it into the
-		// paint panel to diplay it
-		BufferedImage img = null;
-		try {
-			img = ImageIO.read(fl);
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
+        // Read the image and pass it into the
+        // paint panel to diplay it
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(fl);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
-		paint.displayImage(img);
-	}
+        paint.displayImage(img);
+    }
 
-	private void setGUI() {
-		// Add menu
-		buildMenu();
+    private void setGUI() {
+        // Add menu
+        buildMenu();
 
-		// Add toolbar
-		buildToolbar();
+        // Add toolbar
+        buildToolbar();
 
-		// Add the paint panel to our frame
-		frame.add(paint, BorderLayout.CENTER);
-	}
+        // Add the paint panel to our frame
+        frame.add(paint, BorderLayout.CENTER);
+    }
 
     /*
-	* Builds the Menu for the paint app
-	* and sets actions for each menu item.
+    * Builds the Menu for the paint app
+    * and sets actions for each menu item.
     */
-	private void buildMenu() {
-		JMenuBar menuBar = new JMenuBar();
+    private void buildMenu() {
+        JMenuBar menuBar = new JMenuBar();
 
-		/* File menu section */
-		JMenu fileMenu = new JMenu("File");
-		fileMenu.setMnemonic(KeyEvent.VK_F);
+        /* File menu section */
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
 
-		// Save menu item
-		JMenuItem fileSaveItem = new JMenuItem("Save");
-		fileSaveItem.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				save();
-			}
-		});
+        // Save menu item
+        JMenuItem fileSaveItem = new JMenuItem("Save");
+        fileSaveItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                save();
+            }
+        });
 
-		// Open menu item
-		JMenuItem fileOpenItem = new JMenuItem("Open");
-		fileOpenItem.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				load();
-			}
-		});
+        // Open menu item
+        JMenuItem fileOpenItem = new JMenuItem("Open");
+        fileOpenItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                load();
+            }
+        });
 
-		// Exit menu item
-		JMenuItem fileExitItem = new JMenuItem("Exit");
-		fileExitItem.addActionListener(new ExitAction());
-
-
-		fileMenu.add(fileSaveItem);
-		fileMenu.add(fileOpenItem);
-		fileMenu.addSeparator();
-		fileMenu.add(fileExitItem);
-
-		menuBar.add(fileMenu);
-		/* End of file menu section */
+        // Exit menu item
+        JMenuItem fileExitItem = new JMenuItem("Exit");
+        fileExitItem.addActionListener(new ExitAction());
 
 
-		// Add menubar to frame
-		frame.setJMenuBar(menuBar);
-	}
+        fileMenu.add(fileSaveItem);
+        fileMenu.add(fileOpenItem);
+        fileMenu.addSeparator();
+        fileMenu.add(fileExitItem);
 
-	/*
-	* Builds the toolbar and sets event listeners
-	* for buttons and text fields in the toolbar.
-	*/
-	public void buildToolbar() {
-		JToolBar toolbar = new JToolBar();
-		toolbar.setLayout(new FlowLayout(FlowLayout.LEADING));
-		toolbar.setFloatable(false);
+        menuBar.add(fileMenu);
+        /* End of file menu section */
 
-		// Add the exit button
-		JButton exitBtn = new JButton();
-		exitBtn.addActionListener(new ExitAction());
-		exitBtn.setIcon(new ImageIcon("images/exitimg.png"));
-		exitBtn.setToolTipText("Exit");
-		
-		toolbar.add(exitBtn);
 
-		// Clear painting panel button
-		JButton clearButton = new JButton("Clear");
-		clearButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				paint.clean();
-			}
-		});
+        // Add menubar to frame
+        frame.setJMenuBar(menuBar);
+    }
 
-		toolbar.add(clearButton);
+    /*
+    * Builds the toolbar and sets event listeners
+    * for buttons and text fields in the toolbar.
+    */
+    public void buildToolbar() {
+        JToolBar toolbar = new JToolBar();
+        toolbar.setLayout(new FlowLayout(FlowLayout.LEADING));
+        toolbar.setFloatable(false);
 
-		// Change color button
-		JButton colorButton = new JButton();
-		colorButton.setIcon(new ImageIcon("images/color.png"));
-		colorButton.setToolTipText("Choose Color");
+        // Add the exit button
+        JButton exitBtn = new JButton();
+        exitBtn.addActionListener(new ExitAction());
+        exitBtn.setIcon(new ImageIcon("images/exitimg.png"));
+        exitBtn.setToolTipText("Exit");
+        
+        toolbar.add(exitBtn);
 
-		colorButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				Color c = JColorChooser.showDialog(frame, "Select Line Color", 
-														Color.WHITE);
-				// If the returned color is not null
-				// set the line color as the selected color
-				if (c != null) {
-					paint.setColor(c);
-				}
-			}
-		});
+        // Clear painting panel button
+        JButton clearButton = new JButton("Clear");
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                paint.clean();
+            }
+        });
 
-		toolbar.add(colorButton);
+        toolbar.add(clearButton);
 
-		// Pencil Button
-		JButton pencilButton = new JButton();
-		pencilButton.setIcon(new ImageIcon("images/pencil.png"));
-		pencilButton.addActionListener(new ChangeColorListener(Color.BLACK));
+        // Change color button
+        JButton colorButton = new JButton();
+        colorButton.setIcon(new ImageIcon("images/color.png"));
+        colorButton.setToolTipText("Choose Color");
 
-		toolbar.add(pencilButton);
+        colorButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Color c = JColorChooser.showDialog(frame, "Select Line Color", 
+                                                        Color.WHITE);
+                // If the returned color is not null
+                // set the line color as the selected color
+                if (c != null) {
+                    paint.setColor(c);
+                }
+            }
+        });
 
-		// Eraser Button
-		JButton eraserButton = new JButton();
-		eraserButton.setIcon(new ImageIcon("images/eraser.png"));
-		eraserButton.addActionListener(new ChangeColorListener(Color.WHITE));
+        toolbar.add(colorButton);
 
-		toolbar.add(eraserButton);
+        // Pencil Button
+        JButton pencilButton = new JButton();
+        pencilButton.setIcon(new ImageIcon("images/pencil.png"));
+        pencilButton.addActionListener(new ChangeColorListener(Color.BLACK));
 
-		// Set focus event for the sizeField so once
-		// the focus is out of this field the size of 
-		// the line width is changed automatically
-		JTextField sizeField = new JTextField("Size", 10);
+        toolbar.add(pencilButton);
 
-		sizeField.addFocusListener(new FocusListener() {
-			@Override
-			public void focusLost(FocusEvent e) {
-				// Try to change the text we got into float
-				try {
-					float width = Float.parseFloat(sizeField.getText());
-					paint.setLineWidth(width);
-				} catch (NumberFormatException ex) {}
-			}
+        // Eraser Button
+        JButton eraserButton = new JButton();
+        eraserButton.setIcon(new ImageIcon("images/eraser.png"));
+        eraserButton.addActionListener(new ChangeColorListener(Color.WHITE));
 
-			@Override
-			public void focusGained(FocusEvent e) {}
-		});
-		toolbar.add(sizeField);
+        toolbar.add(eraserButton);
 
-		frame.add(toolbar, BorderLayout.NORTH);
-		frame.requestFocus();
-	}
+        // Set focus event for the sizeField so once
+        // the focus is out of this field the size of 
+        // the line width is changed automatically
+        JTextField sizeField = new JTextField("Size", 10);
 
-	// Listener classes
-	private class ChangeColorListener implements ActionListener {
-		Color color;
+        sizeField.addFocusListener(new FocusListener() {
+            @Override
+            public void focusLost(FocusEvent e) {
+                // Try to change the text we got into float
+                try {
+                    float width = Float.parseFloat(sizeField.getText());
+                    paint.setLineWidth(width);
+                } catch (NumberFormatException ex) {}
+            }
 
-		public ChangeColorListener(Color c) {
-			color = c;
-		}
+            @Override
+            public void focusGained(FocusEvent e) {}
+        });
+        toolbar.add(sizeField);
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			paint.setColor(color);
-		}
-	}
+        frame.add(toolbar, BorderLayout.NORTH);
+        frame.requestFocus();
+    }
 
-	private class ExitAction implements ActionListener {
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			frame.setVisible(false);
-			frame.dispose();
-			System.exit(0);
-		}
-	}
+    // Listener classes
+    private class ChangeColorListener implements ActionListener {
+        Color color;
+
+        public ChangeColorListener(Color c) {
+            color = c;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            paint.setColor(color);
+        }
+    }
+
+    private class ExitAction implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            frame.setVisible(false);
+            frame.dispose();
+            System.exit(0);
+        }
+    }
 }
--- PaintPanel.java
+++ PaintPanel.java
@@ -137,4 +137,143 @@ class PaintPanel extends JPanel implements MouseMotionListener, MouseListener{
 			}
 		}
 	}
+}import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.awt.image.BufferedImage;
+
+class PaintPanel extends JPanel implements MouseMotionListener, MouseListener{
+    private ArrayList<ArrayList<Point>> pointList;
+    private ArrayList<Point> currentList;
+    private ArrayList<Float> lineWidthList;
+    private ArrayList<Color> colorList;
+
+    private float lineWidth;
+    private Color lineColor;
+    private BufferedImage loadImage;
+    private boolean firstTimeLoading;
+
+    public PaintPanel() {
+        // Add the mouse listner
+        setBackground(Color.WHITE);
+        setFocusable(true);
+        addMouseListener(this);
+        addMouseMotionListener(this);
+
+        // Initialize the lists that holds all drawn points
+        // colors, and linewidths
+        resetLists();
+
+        // Initialize settings
+        lineWidth = 1;
+        lineColor = Color.BLACK;
+        loadImage = null;
+        firstTimeLoading = false;
+    }
+
+    private void resetLists() {
+        // The list that holds all drawn points
+        pointList = new ArrayList<>();
+        lineWidthList = new ArrayList<>();
+        colorList = new ArrayList<>();
+        currentList = null;
+
+    }
+
+    public void setLineWidth(float width) {
+        lineWidth = width;
+    }
+
+    public void setColor(Color c) {
+        lineColor = c;
+    }
+
+    public void clean() {
+        // Clean the background
+        loadImage = null;
+        resetLists();
+        repaint();
+    }
+
+    public void displayImage(BufferedImage toLoad) {
+        loadImage = toLoad;
+        firstTimeLoading = true;
+        repaint();
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        currentList.add(e.getPoint());
+        // Paint the new point on the screen
+        revalidate();
+        repaint(getBounds());
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {}
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        requestFocusInWindow();
+        currentList = new ArrayList<>();
+        // Add the current line width
+        // and the current color
+        lineWidthList.add(lineWidth);
+        colorList.add(lineColor);
+
+        pointList.add(currentList);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        currentList = null;
+    }
+
+    @Override
+    public void    mouseClicked(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D gg = (Graphics2D) g;
+
+        // Check to see if an image is going to be loaded
+        // if true, reset the lists and display the image
+        // into the panel
+        if (loadImage != null) {
+            if (firstTimeLoading) {
+                resetLists();
+                firstTimeLoading = false;
+            }
+
+            gg.drawImage(loadImage, 0, 0, this);
+        }
+
+        // Draw all the points
+        int index = 0;
+        for (ArrayList<Point> lst: pointList) {
+            Point last = null;
+            // Get the line width for the current list
+            // of drawn points
+            float width = lineWidthList.get(index);
+            Color color = colorList.get(index++);
+            // Set the line width and color
+            gg.setStroke(new BasicStroke(width));
+            gg.setColor(color);
+            for (Point p: lst) {
+                // g.drawRect(p.x, p.y, 1, 1);
+                if (last != null) {
+                    gg.drawLine(last.x, last.y, p.x, p.y);
+                }
+                last = p;
+            }
+        }
+    }
 }
